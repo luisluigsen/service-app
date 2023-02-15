c919342054620f94d6caed4c752ca64f01f63951
luisdev <luisdev@develop.com>
2023-02-15 01:20:58 +0100

    add-tests

--- migrations/dev/Version20230213021005.php
+++ migrations/dev/Version20230213021005.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace DoctrineMigrations;
+namespace DoctrineMigrations\dev;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
---
+++ tests/Functional/Customer/Controller/Customer/CreateCustomerControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Functional\Customer\Controller\Customer;
+
+use App\Tests\Functional\Customer\Controller\CustomerControllerTestBase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateCustomerControllerTest extends CustomerControllerTestBase
+{
+    private const ENDPOINT = '/customer/create';
+
+    public function testCreateController(): void
+    {
+        $payload = [
+            'name' => 'Peter',
+            'address' => 'Fake street 123',
+            'age' => 30,
+            'employeeId' => '78c5b803-e335-43da-b3e2-f8400bbd2907'
+        ];
+
+        $this->client->request(Request::METHOD_POST, self::ENDPOINT, [], [], [], json_encode($payload));
+
+        $response = $this->client->getResponse();
+        $responseData = $this->getResponseData($response);
+
+        self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        self::assertArrayHasKey('customerId', $responseData);
+        self::assertEquals(36, strlen($responseData['customerId']));
+
+        $this->client->request(Request::METHOD_GET, sprintf('/customer/%s', $responseData['customerId']));
+
+        $response = $this->client->getResponse();
+        $responseData = $this->getResponseData($response);
+
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+        self::assertArrayHasKey('name', $responseData);
+        self::assertArrayHasKey('address', $responseData);
+        self::assertArrayHasKey('age', $responseData);
+        self::assertArrayHasKey('employeeId', $responseData);
+
+        self::assertArrayHasKey('name', $responseData['name']);
+        self::assertArrayHasKey('address', $responseData['address']);
+        self::assertArrayHasKey('age', $responseData['age']);
+        self::assertArrayHasKey('employeeId', $responseData['employeeId']);
+    }
+}
