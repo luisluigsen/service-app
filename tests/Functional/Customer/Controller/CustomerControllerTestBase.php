5148bb975719a8fae10da7c3b1d163c18398c2b3
Luis Rodriguez gomez <pipe_spain@hotmail.com>
2023-02-25 04:32:28 +0100

    fixing changes in CustomerControllerTestBase

--- tests/Functional/Customer/Controller/CustomerControllerTestBase.php
+++ tests/Functional/Customer/Controller/CustomerControllerTestBase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Functional\Customer\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -8,21 +10,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CustomerControllerTestBase extends WebTestCase
 {
-    protected ?AbstractBrowser $client;
+    protected AbstractBrowser $client;
 
     public function setUp(): void
     {
         $this->client = static::createClient();
         $this->client->setServerParameter('CONTENT_TYPE', 'application/json');
     }
-     public function tearDown(): void
-     {
-        $this->client= null;
-     }
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
 
-     protected function getResponseData(Response $response): array
-     {
-         return (array) \json_decode($response->getContent(), true, 512, \JSON_THROW_ON_ERROR);
-     }
-
+    protected function getResponseData(Response $response): array
+    {
+        return (array) \json_decode($response->getContent(), true);
+    }
 }
